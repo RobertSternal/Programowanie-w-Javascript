aedfa38da0b2422299a11a140c4d0fe7b98f2324
Robert Sternal <robert.sternal@microsoft.wsei.edu.pl>
2022-10-06 15:01:19 +0200

    06.10 v2

--- main.js
+++ main.js
@@ -6,10 +6,13 @@
 //var wyniki = document.getElementById(wyniki)
 
 const liczba1 = document.querySelector('#liczba1')
+const liczba2 = document.querySelector('#liczba2')
+const liczba3 = document.querySelector('#liczba3')
+const liczba4 = document.querySelector('#liczba4')
 const buttonprzelicz = document.querySelector('#przelicz')
 const wynikiPojemnik = document.querySelector('#wyniki')
 
 buttonprzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML=liczba1.value
+    wynikiPojemnik.innerHTML=liczba1.value+liczba2.value+liczba3.value+liczba4.value
     console.dir(liczba1.value)
 })
