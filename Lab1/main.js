cd08ebcd9b704a1287de0e15f0802d0df9a221bf
RobertSternal <robst@onet.pl>
2022-10-11 19:47:55 +0200

    lab1 v2

--- Lab1/main.js
+++ Lab1/main.js
@@ -1,18 +1,17 @@
-//var liczba1= document.getElementById(liczba1)
-//var liczba2= document.getElementById(liczba2)
-//var liczba3= document.getElementById(liczba3)
-//var liczba4= document.getElementById(liczba4)
-//var buttonprzelicz= document.getElementById(przelicz)
-//var wyniki = document.getElementById(wyniki)
-
 const liczba1 = document.querySelector('#liczba1')
 const liczba2 = document.querySelector('#liczba2')
 const liczba3 = document.querySelector('#liczba3')
-const liczba4 = document.querySelector('#liczba4')
-const buttonprzelicz = document.querySelector('#przelicz')
-const wynikiPojemnik = document.querySelector('#wyniki')
 
-buttonprzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML=liczba1.value+liczba2.value+liczba3.value+liczba4.value
-    console.dir(liczba1.value)
+const sumaWynik = document.querySelector('#suma')
+const sredniaWynik = document.querySelector('#srednia')
+const minWynik = document.querySelector('#min')
+const maxWynik = document.querySelector('#max')
+
+const przeliczBtn = document.querySelector('#przelicz')
+
+przeliczBtn.addEventListener('click', () => {
+    sumaWynik.innerHTML = 'Suma: '+ (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value))
+    sredniaWynik.innerHTML = 'Srednia: ' + (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value))/3
+    minWynik.innerHTML = 'Min: ' + Math.min(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value))
+    maxWynik.innerHTML = 'Max: ' + Math.max(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value))
 })
